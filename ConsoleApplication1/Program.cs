e754bd40e58a649959ea1f00025f867d4534cfa5
Grant <ghwilley@gmail.com>
2016-09-25 15:57:44 -0500

    cleared for namespace work

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,29 +8,11 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        private static string k = "";
-
         static void Main(string[] args)
         {
-            int r;
-            r =  0;
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-                k = i.ToString();
-                r = i;
-            }
-            Console.WriteLine(k);
-
-            HelperMethod();
+            Console.WriteLine("HELLO");
             Console.ReadLine();
         }
-
-        static void HelperMethod()
-        {
-            Console.WriteLine("ValueType of K " + k);
-        }
-
     }
 
 }
