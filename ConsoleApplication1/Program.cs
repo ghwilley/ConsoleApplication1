6548d666e0dce4ec31640d85d45a064b1741c0dd
Grant <ghwilley@gmail.com>
2016-09-25 14:17:19 -0500

    removed car for clean work

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,48 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car();
-
-
-            //myCar.Make = "Oldsmobile";
-            //myCar.Model = "Ark";
-            //myCar.Year = 1989;
-            //myCar.Color = "Red";
-
-            Car myOtherCar;
-            myOtherCar = myCar;
-
-            Console.WriteLine("{0} {1} {2} {3}",
-                
-                myOtherCar.Make,
-                myOtherCar.Model,
-                myOtherCar.Year,
-                myOtherCar.Color);
-
-            myOtherCar.Model = "98";
-
-            Console.WriteLine("{0} {1} {2} {3}",
-
-                myCar.Make,
-                myCar.Model,
-                myCar.Year,
-                myCar.Color);
-             
-            Console.ReadLine();
+           
         }
 
     }
 
-    class Car
-    {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string Color { get; set; }
-
-        public Car()
-        {
-            this.Make = "Nissan";
-        }
-    }
+    
 }
