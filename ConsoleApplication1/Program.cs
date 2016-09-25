6027e2cd7feb533abc8fff5b9fe58f528e610834
Grant <ghwilley@gmail.com>
2016-09-25 17:40:14 -0500

    scraped webclient shit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace ConsoleApplication1
 {
@@ -11,9 +12,16 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string text = "A class is the most powerful data type in C#. Like a structure, " +
-                       "a class defines the data and behavior of the data type. ";
-            File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", text);
+           // string text = "A class is the most powerful data type in C#. Like a structure, " +
+           //            "a class defines the data and behavior of the data type. ";
+          //  File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", text);
+
+            string address = "http://www.google.com";
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+
+            Console.WriteLine(reply);
 
             Console.ReadLine();
         }
