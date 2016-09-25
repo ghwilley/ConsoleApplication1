7cc80cf6220945e1dbd508feee62a600237e79d1
Grant <ghwilley@gmail.com>
2016-09-25 15:55:22 -0500

    learnin some scope tricks

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,12 +8,29 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        private static string k = "";
+
         static void Main(string[] args)
         {
-           
+            int r;
+            r =  0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                k = i.ToString();
+                r = i;
+            }
+            Console.WriteLine(k);
+
+            HelperMethod();
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("ValueType of K " + k);
         }
 
     }
 
-    
 }
